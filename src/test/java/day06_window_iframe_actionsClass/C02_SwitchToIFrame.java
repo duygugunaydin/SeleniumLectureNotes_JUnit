e3163ea7948b220357fb845fdb73dd546f2ae28f
duygugunaydin <dyg.gunaydin@gmail.com>
2023-02-21 23:22:06 +0300

    21/02/2023

--- src/test/java/day06_window_iframe_actionsClass/C02_SwitchToIFrame.java
+++ src/test/java/day06_window_iframe_actionsClass/C02_SwitchToIFrame.java
@@ -1,14 +1,13 @@
-package day06;
+package day06_window_iframe_actionsClass;
 
-import org.checkerframework.checker.units.qual.A;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import utilities.TestBaseClass;
+import utilities.TestBase;
 
-public class c02_Iframe extends TestBaseClass {
+public class C02_SwitchToIFrame extends TestBase {
+
 
     @Test
     public void test01(){
@@ -20,21 +19,43 @@ public class c02_Iframe extends TestBaseClass {
             Locator calismaz
             Bunun sebebi kullanmak istedigimiz webelement'in
             bir iframe'in child'i olmasi olabilir.
+
             Eger Kullanmak istedigimiz webelement
             anasayfa icine gomulen baska bir websayfasinin elementi ise
             once gomulu sayfaya gecmemiz gerekir
+
             HTML komutlarinda sayfa icerisine sayfa gomebilmek icin
             <iframe> kullanilir
             Biz de istedigimiz elementi kullanmak icin once
             o frame'e switch yapmaliyiz
+
             1- gecis yapmak istediginiz frame'i locate edin
             2- switchTo() ile o frame'e gecis yapin
             3- Sonra istediginiz element ile istediginiz islemi yapin
          */
         WebElement frameElementi= driver.findElement(By.id("mce_0_ifr"));
         driver.switchTo().frame(frameElementi);
+
+
         WebElement yaziElementi = driver.findElement(By.tagName("p"));
         Assert.assertTrue(yaziElementi.isDisplayed());
+
+        // Elemental Selenium linkine click yapin
+        /*
+            Bir frame'e gecis yaptiktan sonra
+            oradan cikincaya veya baska bir frame'e gecinceye kadar
+            driver o frame icerisinde kalir
+
+            isimiz bittiginde veya baska frame'e gecmek istedigimizde
+            once calisacagimiz yere gecis yapmaliyiz
+         */
+
+        driver.switchTo().parentFrame(); // icice frame'lerde bir ust seviyeye cikar
+        //driver.switchTo().defaultContent();// direk anasayfaya cikar
+        driver.findElement(By.linkText("Elemental Selenium")).click();
+
+
         bekle(3);
+
     }
 }
